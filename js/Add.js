77e51af6998ffc2798e26d08dabd8adbc439e914
gooner16 <lukec978@gmail.com>
2015-03-10 14:55:08 -0400

    Add frame

--- js/Add.js
+++ js/Add.js
@@ -5,8 +5,12 @@ function AddRowFunction() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    cell1.innerHTML = "[Market Name]";
+    var yourSelect = document.getElementById( "selectBox" );
+    
+        
+    cell1.innerHTML = yourSelect.options[ yourSelect.selectedIndex ].value;
     cell2.innerHTML = "[Value]";
     cell3.innerHTML = "[Change]";
     cell4.innerHTML = '<input type="button" value="X" onclick="deleteRow(this)">';
 }
+
