048b98bf73fecda02036f8a2b78ffc39e9333d3d
gooner16 <lukec978@gmail.com>
2015-03-11 01:05:48 -0400

    Formatted Syntax and File Structure
    Organization

--- unused/AddJQuery.js
+++
@@ -1,5 +0,0 @@
-	 $(document).ready(function(){
-	 $("#add").click(function(){
-	 $('#table1 tr').last().after('<tr><td width="25%"></td><td width="25%"></td><td width="23%"></td><td width="2%"><input type="button" value="X" onclick="deleteRow(this)"></td></tr>');
-	 });	 
-	 });
