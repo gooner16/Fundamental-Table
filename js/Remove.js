fa38465e0eb8f4f5e2fbe3381b3b10ae1080801e
GitHubWhatIsIt <richardrencn@163.com>
2015-03-14 22:57:04 -0400

    Debugging 03/14

--- js/Remove.js
+++ js/Remove.js
@@ -1,4 +1,8 @@
 function deleteRow(r) {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById("table1").deleteRow(i);
+	x--
+	//for "x", please see "Add.js"
+	executed1=false
+	//
 }
