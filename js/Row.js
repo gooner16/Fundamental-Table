77cca7360bad1b0ac9127ca271adfbed7aba1a35
GitHubWhatIsIt <richardrencn@163.com>
2015-04-07 14:38:59 -0400

    A bug is in here.

--- js/Row.js
+++ js/Row.js
@@ -8,10 +8,8 @@ var array = ["Assets","Book Value","Capital Expenditures","Cash","Cost of Good S
 
 function AppendOptions1(me,num,ind){
 	if (!(arrayExe[num])) {
-		//me.remove(0);
-		//Remove first option of selectbox
-
-		var array = ["Assets","Book Value","Capital Expenditures","Cash","Cost of Good Sold","DPS","EPS Base","EPS Diluted","Float Shares","Good Will","Income After Tax","Income Before Tax","Institutional Own","Inventory","Liability","Long Term Debt","Net Income","Number of Employees","Operating Cash Flow","Operating Income","Revenue","Short Term Debt","Total Operating Expense"];
+		
+		var array = ["Assets","Book Value","Capital Expenditures","Cash","Cost of Good Sold","DPS","EPS Base",/*"EPS Diluted",*/"Float Shares","Good Will","Income After Tax","Income Before Tax","Institutional Own","Inventory","Liability","Long Term Debt","Net Income","Number of Employees","Operating Cash Flow",/*"Operating Income",*/"Revenue","Short Term Debt","Total Operating Expense"];
 
 		//Create array of options to be added
 		for (var i = 0; i < array.length; i++) {
@@ -31,51 +29,52 @@ function AppendOptions1(me,num,ind){
 function AddRowFunction(){
 
 	Name = $('#autocomplete')[0].value
-	if ( Name != "" ){
-
-	var yourSelect = document.getElementById( "selectBox" );
-	var option = document.createElement("option");
-		option.value = yourSelect.options[ yourSelect.selectedIndex ].value;
-		option.text = yourSelect.options[ yourSelect.selectedIndex ].value;
-	var Index = yourSelect.selectedIndex;
-		//the option would be the one selected on selectBox
 	
-	if (option.value != "Fundamentals") {
-		var table = document.getElementById("table1");
-		var row = table.insertRow(2+x);
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
-		var cell4 = row.insertCell(3);
-		x++ //Next row will be added below this one
-		y++ //This is used to assign a different string to every selectBox
-	
-	var boxValue
-	for ( var k = 0; k<array.length ; k ++){
-	if ( yourSelect.options[ yourSelect.selectedIndex ].value == array[k] && option.value != "Fundamentals"){
-		 boxValue = fundata[k]
-		break
+	if (Name == "" || Name == "Stock Name" ) {
+		document.getElementById("alertStock").innerHTML = "Please enter a stock ticker."
 	}
-}
-		cell1.innerHTML = '<select id = "selectBox0">';
-		cell2.innerHTML = boxValue;
-		cell3.innerHTML = "[Change]";
-		cell4.innerHTML = '<input type = "button" value = "X" onclick = "deleteRow(this)">';
-
-		AppendOptions1(selectBox0,y,Index)
-		//Puting the function here will bypass the "onclick" event
+	else {
+		var yourSelect = document.getElementById( "selectBox" );
+		var option = document.createElement("option");
+			option.value = yourSelect.options[ yourSelect.selectedIndex ].value;
+			option.text = yourSelect.options[ yourSelect.selectedIndex ].value;
+		var Index = yourSelect.selectedIndex;
+			//the option would be the one selected on selectBox
 		
-		var newId = "selectBox" + y ;
-		//This will be the Id for the selectBox in row
-		document.getElementById("selectBox0").id = newId;
-		//Give select box a new id
-		arrayExe[y] = false;
-	}
-	//alert(document.getElementById("selectBox5").id)
+		if (option.value != "Fundamentals") {
+			var table = document.getElementById("table1");
+			var row = table.insertRow(2+x);
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+			x++ //Next row will be added below this one
+			y++ //This is used to assign a different string to every selectBox
+			
+			var boxValue
+			for ( var k = 0; k<array.length ; k ++) {
+				if ( yourSelect.options[ yourSelect.selectedIndex ].value == array[k] && option.value != "Fundamentals") {
+				boxValue = fundata[k]
+				break
+				}
+			}
+		
+				cell1.innerHTML = '<select id = "selectBox0">';
+				cell2.innerHTML = boxValue.val;
+				cell3.innerHTML = "[Change]";
+				cell4.innerHTML = '<input type = "button" value = "X" onclick = "deleteRow(this)">';
 
-	}else{
-	alert("Please Enter a Stock Name")
-}
+				AppendOptions1(selectBox0,y,Index)
+				//Puting the function here will bypass the "onclick" event
+				
+				var newId = "selectBox" + y ;
+				//This will be the Id for the selectBox in row
+				document.getElementById("selectBox0").id = newId;
+				//Give select box a new id
+				arrayExe[y] = false;
+		}
+	}
+	
 }
 
 var executed0 = false
