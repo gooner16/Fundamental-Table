bbaf53813fddb7cb1d7f2f5b9430323f5a4ec839
GitHubWhatIsIt <richardrencn@163.com>
2015-04-09 14:51:20 -0400

    Last commit before 3

--- js/Row.js
+++ js/Row.js
@@ -12,10 +12,7 @@ function AddRowFunction(){
 	Name =$ ('#autocomplete')[0].value;
 	var yourSelect = document.getElementById( "selectBox" );
 	
-	if (!(Name == "" || Name == "Stock Name" ))
-	{
-		document.getElementById("alertStock").innerHTML = "";
-
+	if (!(Name == "" || Name == "Stock Name" )) {
 		var option = document.createElement("option");
 			option.value = yourSelect.options[ yourSelect.selectedIndex ].value;
 			option.text = yourSelect.options[ yourSelect.selectedIndex ].value;
@@ -35,26 +32,27 @@ function AddRowFunction(){
 			
 			var boxValue
 			for ( var k = 0; k<fundata.length ; k ++) {
-				if ( /*yourSelect.options[ yourSelect.selectedIndex ].value*/ option.value == fundata[k].label && option.value != "Fundamentals") {
+				if (option.value == fundata[k].label && option.value != "Fundamentals") {
 				boxValue = fundata[k];
 				break;
 				}
 			}
 
 				cell1.innerHTML = '<select id = "selectBox0" onchange = "Swagger(this, this.id)">';
-				cell2.innerHTML = "<p id = 'lable0'>";
+				cell2.innerHTML = "<p id = 'label0'>";
 				cell3.innerHTML = "[Change]";
 				cell4.innerHTML = '<input type = "button" value = "X" onclick = "deleteRow(this)">';
 
 				AppendOptions1(selectBox0,y,Index);
 				//Puting the function here will bypass the "onclick" event
-				var tempyee= document.getElementById("lable0")
+				var tempyee= document.getElementById("label0")
 				tempyee.innerHTML = boxValue.val;
 				var newId = "selectBox" + y ;
-				var newId2 = "lable"  + y
+				var newId2 = "label"  + y
 				//This will be the Id for the selectBox in row
+
 				document.getElementById("selectBox0").id = newId;
-				document.getElementById("lable0").id = newId2;
+				document.getElementById("label0").id = newId2;
 
 				//Give select box a new id
 				arrayExe[y] = false;
@@ -69,12 +67,6 @@ function AddRowFunction(){
 
 function AppendOptions1(me,num,ind){
 	if (!(arrayExe[num])) {
-		//me.remove(0);
-		//Remove first option of selectbox
-
-		var array = ["Assets","Book Value","Capital Expenditures","Cash","Cost of Good Sold","DPS","EPS Base",/*"EPS Diluted",*/"Float Shares","Good Will","Income After Tax","Income Before Tax","Institutional Own","Inventory","Liability","Long Term Debt","Net Income","Number of Employees",/*"Operating Cash Flow",*/"Operating Income","Revenue","Short Term Debt","Total Operating Expense"];
-
-		//Create array of options to be added
 		for (var i = 0; i < array.length; i++) {
 			var option = document.createElement("option");
 			option.value = array[i];
@@ -88,6 +80,7 @@ function AppendOptions1(me,num,ind){
 	//This would give you selected option to become the one actually being selected, and it bypass the bug.
 
 }
+
 //deletes current row
 function deleteRow(r) {
 	var i = r.parentNode.parentNode.rowIndex;
@@ -95,6 +88,7 @@ function deleteRow(r) {
 	x--;
 	//for "x", please see above
 }
+
 //clears all added rows
 function deleteRowAll() {
 	for (var i = 0; i < x; i++) {
@@ -116,6 +110,6 @@ var myString = d2;
 var stringLength = myString.length; // this will be 16
 var lastChar = myString.charAt(stringLength - 1); // this will be the string "."
 //'alert(boxValue)
-document.getElementById("lable" + lastChar).innerHTML= boxValue
+document.getElementById("label" + lastChar).innerHTML= boxValue
 
 }
